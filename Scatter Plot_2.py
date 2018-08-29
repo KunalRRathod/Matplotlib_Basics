e8f9e38c6363bbb332abe618707427843cfcc5b4
Kunal Rathod <36470138+KunalRRathod@users.noreply.github.com>
2018-08-29 19:59:58 +0530

    Update Scatter Plot_2.py

--- Scatter Plot_2.py
+++ Scatter Plot_2.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt; import importlib; importlib.reload(plt)
 import pandas as pd
 plt.clf()
-
-df = pd.read_csv('http://assets.datacamp.com/course/intermediate_python/gapminder.csv', index_col = 0)
+#any dataset as CSV
+df = pd.read_csv('.csv', index_col = 0)
 pop = list(df['population']/1000000)
 life_exp = list(df.life_exp)
 
