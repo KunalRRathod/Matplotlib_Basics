13b9ca383dd2c27f7c9646cf8d359eeabda08157
Kunal Rathod <36470138+KunalRRathod@users.noreply.github.com>
2018-08-29 19:54:33 +0530

    Update Histogram_1.py

--- Histogram_1.py
+++ Histogram_1.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt; import importlib; importlib.reload(plt)
 import pandas as pd
 plt.clf()
-df = pd.read_csv('http://assets.datacamp.com/course/intermediate_python/gapminder.csv', index_col = 0)
+#Any CSV dataset 
+df = pd.read_csv('.csv', index_col = 0)
 life_exp = list(df.life_exp)
 
 # Create histogram of life_exp data
